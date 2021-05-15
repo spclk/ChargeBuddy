b552081a7614b906c8c0f07cbd81fae1d7d9ebf9
Raffee <Raffee1989@gmail.com>
2021-05-15 12:23:00 -0700

    Created components folder and added the header

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
-import Main from "./components/pages/main"
-import Footer from "./components/Footer";
-
-import "./App.css";
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
 
 function App() {
-
-  useEffect(() => {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, {});
-  },[]);
-
-  return (
-    <div>
-      <Navbar />
-      <Router>
-        <Switch>
-          <Route exact path= "/" component={Main}/>
-        </Switch>
-      </Router>
-      <Footer />
-    </div>
-  );
-
+  return <Navbar />;
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Charge Buddy</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">My Garage</a></li>
+            </ul>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
