4b42ca6e40cb41f8a04c01f7dfd6931d71a84b9a
warren clark <wclarkcito@gmail.com>
2021-05-15 11:55:12 -0700

    renamed file in models

--- client/models/user.js
+++ client/models/user.js
@@ -23,7 +23,7 @@ const userSchema = new Schema({
             },
             userCreated: {
                 type: Date,
-                default: Date.now
+                default: Date.no
             }
 
         }
