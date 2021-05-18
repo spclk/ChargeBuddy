bedceae9ec14fb9a015d154c73e78bf4ee309a3c
Konstantin <konstantin1020@gmail.com>
2021-05-18 13:50:12 -0700

    installed package for Materialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React, {useEffect}  from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+// importing Materialize.css
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
+// importing Components
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 
 function App() {
 
+  // do not remove 
+  // useEffect is needed for Materialize JS to work
   useEffect(() => {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, {});
+    var elems = document.querySelectorAll('');
+    var instances = M.init(elems, {});
   },[]);
 
   return (
+    // Router needed to navigate between pages
     <Router>
       <div>
         <Navbar />
