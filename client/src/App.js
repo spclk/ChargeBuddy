f82bdafcca441554d76da4d14fa1868eeb4f43a7
Konstantin <konstantin1020@gmail.com>
2021-05-18 13:30:29 -0700

    set up Materialize

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 
 function App() {
+
+  useEffect(() => {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+  },[]);
+  
   return (
     <Router>
       <div>
