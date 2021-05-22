ef58e50addb281eacbcea0a1af73f425d5b07aa1
Sarness Prasad <sunny776@yahoo.com>
2021-05-22 10:20:37 -0700

    fixed server.js and index.js in api folder

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,13 @@ const apiRoutes = require("./api");
 const carRoute = require("./car");
 const userRoute = require("./user");
 
-
-
-
 // API Routes
 router.use("/api", apiRoutes);
-router.use("/",carRoutes);
-router.use("/",userRoutes);
+router.use("/", carRoutes);
+router.use("/", userRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
-  });
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 module.exports = router;
