7183e33ec1b52a96a2464819e5b4e02ef6cfb777
Raffee <Raffee1989@gmail.com>
2021-05-22 11:16:40 -0700

    changed the classes to classNames

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,18 +3,18 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="page-footer card-content valign center">
-      <div class="nav-content">
-        <ul class="tabs tabs-transparent">
-          <li class="tab">
+      <div className="nav-content">
+        <ul className="tabs tabs-transparent">
+          <li className="tab">
             <a href="#test1">Test 1</a>
           </li>
-          <li class="tab">
+          <li className="tab">
             <a href="#test2">Test 2</a>
           </li>
-          <li class="tab">
+          <li className="tab">
             <a href="#test1">Test 3</a>
           </li>
-          <li class="tab">
+          <li className="tab">
             <a href="#test4">Test 4</a>
           </li>
         </ul>
