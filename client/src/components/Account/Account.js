547eeb54b645f4788aa900650388addb2ab3ca86
Konstantin <konstantin1020@gmail.com>
2021-05-27 14:04:45 -0700

    added content to Account.js component

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -3,6 +3,76 @@ import React from "react";
 const Account = () => {
   return (
     <div>
+      <main>
+        <div className="section container">
+          <div className="row">
+            <div className="col s12 m4">
+              <span className="settings-title">
+                <h5>Personal Information</h5>
+              </span>
+              <p>your info</p>
+            </div>
+
+            <div className="col s12 m8">
+              <ul className="collapsible">
+                <li>
+                  <div className="collapsible-header">Name</div>
+                  <div className="collapsible-body">
+                    <span>Lorem ipsum dolor sit amet.</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="collapsible-header">Email</div>
+                  <div className="collapsible-body">
+                    <span>Lorem ipsum dolor sit amet.</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="collapsible-header">Home Location</div>
+                  <div className="collapsible-body">
+                    <span>Lorem ipsum dolor sit amet.</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col s12 m4">
+              <span className="settings-title">
+                <h5>Vehicle Information</h5>
+              </span>
+              <p>vehicle info</p>
+            </div>
+
+            <div className="col s12 m8">
+              <ul className="collapsible">
+                <li>
+                  <div className="collapsible-header">Vehicle</div>
+                  <div className="collapsible-body">
+                    <span>Lorem ipsum dolor sit amet.</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="collapsible-header">Plug Type</div>
+                  <div className="collapsible-body">
+                    <span>Lorem ipsum dolor sit amet.</span>
+                  </div>
+                </li>
+                <li>
+                  <div className="collapsible-header">Charging</div>
+                  <div className="collapsible-body">
+                    <span>Lorem ipsum dolor sit amet.</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+      <p>Now that you're part of the ChargeBuddy community, 
+        start pluggin in prices and tips</p>
+      <a className="waves-effect waves-light btn-large">MAP</a>
       <h1>My car information</h1>
     </div>
   );
