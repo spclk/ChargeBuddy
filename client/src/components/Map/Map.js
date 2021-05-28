5332498a2bc5cf03bb939c2afe30db2a0ea5fe48
EJ <ej.roux13@gmail.com>
2021-05-28 06:09:10 -0700

    stil working on adding markers to map

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 // import dotenv from "dotenv";
+import API from '../../utils/API';
 import "./style.css"
 // import Mapbox
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
@@ -13,6 +14,7 @@ export default function Map() {
     const [zoom, setZoom] = useState(15);
     const [status, setStatus] = useState(null);
     
+    // The user must first allow the browser to use location services
     const getLocation = () => {
         if (!navigator.geolocation) {
           setStatus('Geolocation is not supported by your browser');
@@ -22,6 +24,7 @@ export default function Map() {
             setStatus("Located!");
             setLat(position.coords.latitude);
             setLng(position.coords.longitude);
+            // Once the user allows for location services then the map will zoom in on their location
             map.current.flyTo({
                 center: [position.coords.longitude, position.coords.latitude],
                 essential: true
@@ -51,21 +54,79 @@ export default function Map() {
         });
     });
 
-    // useEffect(() => {
-    //     if (!map.current) return; // wait for map to initialize
-    //     map.current.on('move', () => {
-    //         setLng(lng);
-    //         setLat(lat);
-    //         setZoom(map.current.getZoom().toFixed(2));
-    //     });
-    // }, [lng, lat]);
-     
+    // Extract data from API to create markers for charging stations
+    useEffect(() => {
+        API.getChargingStations().then(res => {
+            console.log("full response", res.data);
+
+            let features = res.data.features;
+            // Create empty array to store geoJSON
+            let geojson = [];
+            for (let i = 0; i < features.length; i++) {
+                // console.log(features[i]);
+                //extracting data from API
+                let lng = features[i].geometry.coordinates[0];
+                let lat = features[i].geometry.coordinates[1];
+                let portType = features[i].properties.ev_connector_types;
+                let name = features[i].properties.station_name;
+                let city = features[i].properties.city;
+                let state = features[i].properties.state;
+                let address = features[i].properties.street_address;
+                let zip = features[i].properties.zip;
+                // Push API data into geoJSON array
+                geojson.push({
+                    'type': 'FeatureCollection',
+                    'features': [
+                    {
+                        'type': 'Feature',
+                        'geometry': {
+                            'type': 'Point',
+                            'coordinates': [lng, lat]
+                        },
+                        'properties': {
+                            'name': name,
+                            'address': address,
+                            'city': city,
+                            'state': state,
+                            'zip': zip,
+                            'portTypes': portType
+                        }
+                    },]
+                });
+            };
+            //console.log(geojson[0].features);
+            // add markers to map
+            for (var i = 0; i < geojson.length; i++){
+                geojson[i].features.forEach(function (marker) {
+                    // create a HTML element for each feature
+                    var el = document.createElement('div');
+                    el.className = 'marker';
+         
+                    // make a marker for each feature and add it to the map
+                    new mapboxgl.Marker(el)
+                        .setLngLat(marker.geometry.coordinates)
+                        .setPopup(
+                            new mapboxgl.Popup({ offset: 25 }) // add popups
+                                .setHTML(
+                                    '<h3>' +
+                                    marker.properties.title +
+                                    '</h3><p>' +
+                                    marker.properties.description +
+                                    '</p>'
+                                )
+                        )
+                        .addTo(this.map);
+                });
+            }
+        })
+    });
+
     return (
         <div>
             <div className="sidebar">
                 Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} | Status: {status}
             </div>
-            <button onClick={getLocation} className="waves-effect waves-light btn"><i class="material-icons left">location_searching</i> Get Location</button>
+            <button onClick={getLocation} className="waves-effect waves-light btn"><i className="material-icons left">location_searching</i> Get Location</button>
             <div ref={mapContainer} className="map-container" />
         </div>
     );
