576424da084cc5a22f0e37803bdd469f6a4914e3
EJ <ej.roux13@gmail.com>
2021-05-27 20:13:04 -0700

    updated model

--- models/user.js
+++ models/user.js
@@ -3,53 +3,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // still need to finish the id object auto increment and primary KeyboardEvent. Create Date object
 
-
 const userSchema = new Schema({
-
-
-    first_name: {
-        type: String,
-        unique: true,
-
-    },
-    last_name: {
-        type: String,
-        unique: true,
-
-    },
-    zip_code: {
-        type: Number,
-        unique: false,
-
-    },
-    email: {
-        type: String,
-        unique: true,
-        match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: "Password is Required",
-        validate: [({ length }) => length >= 8, "Password should be longer."]
-    },
-    userCreated: {
+    day: {
         type: Date,
         default: Date.now
     },
+    users: [
+        {
+            email: {
+                type: String,
+                unique: true,
+                match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+            },
+            password: {
+                type: String,
+                trim: true,
+                required: "Password is Required",
+                validate: [({ length }) => length >= 8, "Password should be longer."]
+            },
+            userCreated: {
+                type: Date,
+                default: Date.now
+            }
 
-    car: {
-        type: Array,
-        default: []
-    }
-
-
-
-
-
-
-
-
+        }
+    ]
 },
     {
         toJson: {
