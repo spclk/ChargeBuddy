04f05c0713a8b145713dfe4f1ded09248cb97701
warren clark <wclarkcito@gmail.com>
2021-05-27 20:22:46 -0700

    added user.js file

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     User: require("./User"),
-    Car: require("./car")
+
 }
---
+++ models/user.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs')
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        first_name: {
+            type: String,
+            unique: true,
+        },
+        last_name: {
+            type: String,
+            unique: true,
+        },
+        zip_code: {
+            type: Number,
+            unique: false,
+        },
+        email: {
+            type: String,
+            unique: true,
+            match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+        },
+        password: {
+            type: String,
+            trim: true,
+            required: "Password is Required",
+            validate: [({ length }) => length >= 8, "Password should be longer."],
+        },
+        userCreated: {
+            type: Date,
+            default: Date.now,
+        },
+        car: {
+            make: String,
+            model: String,
+            year: Number,
+            evPort: String
+        }
+    },
+    {
+        toJson: {
+            virtuals: true,
+        }
+    });
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+});
+
+
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
