96ccad1adb762917a01baeaa4a9a7623422a1ff0
Raffee <Raffee1989@gmail.com>
2021-05-27 20:10:52 -0700

    getting data

--- models/user.js
+++ models/user.js
@@ -1,69 +1,39 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs')
 
 const Schema = mongoose.Schema;
+// still need to finish the id object auto increment and primary KeyboardEvent. Create Date object
 
-const userSchema = new Schema(
-  {
-    first_name: {
-      type: String,
-      unique: true,
+const userSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
     },
-    last_name: {
-      type: String,
-      unique: true,
-    },
-    zip_code: {
-      type: Number,
-      unique: false,
-    },
-    email: {
-      type: String,
-      unique: true,
-      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-    },
-    password: {
-      type: String,
-      trim: true,
-      required: "Password is Required",
-      validate: [({ length }) => length >= 8, "Password should be longer."],
-    },
-    userCreated: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  {
-    toJson: {
-      virtuals: true,
-    },
-  }
-);
-    
-    car: {
-        type: Array,
-        default: []
-    }
+    users: [
+        {
+            email: {
+                type: String,
+                unique: true,
+                match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+            },
+            password: {
+                type: String,
+                trim: true,
+                required: "Password is Required",
+                validate: [({ length }) => length >= 8, "Password should be longer."]
+            },
+            userCreated: {
+                type: Date,
+                default: Date.now
+            }
 
-
-
-
-
-
-
-
-});
-userSchema.pre("save", async function (next) {
-    const user = this;
-
-    if (user.isModified("password")) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
-
-    next();
-});
-
-
+        }
+    ]
+},
+    {
+        toJson: {
+            virtuals: true
+        }
+    });
 
 const User = mongoose.model("User", userSchema);
 
