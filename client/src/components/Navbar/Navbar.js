c65d2d445938fe0dd1eb38642c7d243436bb4611
Konstantin <konstantin1020@gmail.com>
2021-05-28 20:06:49 -0700

    added icons to navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./Navbar.css"
 
 const Navbar = () => {
     return (
         <nav>
             <div className="nav-wrapper">
-                <a href="/" className="brand-logo">Charge Buddy</a>
+                <a href="/" className="brand-logo"><i className="large material-icons">ev_station</i>ChargeBuddy</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="/login">Account</a></li>
+                    <li><a href="/login"><i className="material-icons">person</i></a></li>
                 </ul>
             </div>
         </nav>
