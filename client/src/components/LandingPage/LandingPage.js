b97241942bd2ce1b05d15889b42fb20da76ed1c6
Konstantin <konstantin1020@gmail.com>
2021-05-29 22:31:57 -0700

    finalized button settings

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -18,14 +18,15 @@ const LandingPage = () => {
         <div className={classes.Content}>
           <div className={classes.SubContent}>
             <h3>Need a recharge?</h3>
-            <p>
+            <p className="container">
               Is your Electric Vehicle depleted? Have a sinking feeling in your
               stomach? You're not going to make it. In your mind charging
               stations would be everywhere! How much does it cost? You're now on
               borrowed time and your middle aged road trip will soon be a
-              tragedy that others joke about at your next party. It doesn't have
-              to be this way!
+              tragedy that others joke about at your next party. 
             </p>
+            <br/>
+            <p>It doesn't have to be this way!</p>
           </div>
         </div>
       </div>
@@ -41,13 +42,15 @@ const LandingPage = () => {
         <div className={classes.Content}>
           <div className={classes.SubContent}>
             <h3>ChargeBuddy is your answer!</h3>
-            <p>
-            Charge Buddy offers geolocation as well as peer to peer networking allowing our users to update pricing information.  Locations of all  Charging Stations in your vicinity are displayed on your map, making sure that you arrive at your destination with your head held high. Once you create an account with us your information is securely stored on our server.  You will also be able to update pricing saving other unfortunates from being the punchline at Thanksgiving.  Charge Buddy is not the friend you want but the friend you need. 
+            <p className="container">
+            Charge Buddy offers geolocation as well as peer to peer networking allowing our users to update pricing information.  Locations of all  Charging Stations in your vicinity are displayed on your map, making sure that you arrive at your destination with your head held high. Once you create an account with us your information is securely stored on our server.  You will also be able to update pricing saving other unfortunates from being the punchline at Thanksgiving.
             </p>
-            {/* <button type="button" className="btn btn-outline-dark">
+            <br/>
+            <p> Charge Buddy is not the friend you want but the friend you need!</p>
+            <button type="button">
               START NOW
-            </button> */}
-            <a className="waves-effect waves-light btn-large">Button</a>
+            </button>
+            {/* <a className="waves-effect waves-light btn-large orange darken-2">START NOW</a> */}
           </div>
         </div>
       </div>
