13c9fb9d1685b305a358fc5041ce237229c1ad67
warren clark <wclarkcito@gmail.com>
2021-05-31 14:01:16 -0700

    added session to userController.  fixed bug where vehicle information was not populating in Account.js

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -8,7 +8,7 @@ const LandingPage = () => {
 
   return (
     <main>
-        {/* first video content */}
+      {/* first video content */}
       <div className={classes.Container}>
         <video autoPlay="autoplay" loop="loop" muted className={classes.Video}>
           <source src={firstVideo} type="video/mp4" />
@@ -19,20 +19,19 @@ const LandingPage = () => {
           <div className={classes.SubContent}>
             <h3>Need a recharge?</h3>
             <p className="container">
-              Is your Electric Vehicle depleted? Have a sinking feeling in your
-              stomach? You're not going to make it. In your mind charging
-              stations would be everywhere! How much does it cost? You're now on
-              borrowed time and your middle aged road trip will soon be a
-              tragedy that others joke about at your next party. 
+              Is your Electric Vehicle depleted? You keep passing Gas Stations,
+              but Charging Stations are nowhere to be found and your stomach has
+              developed a sinking feeling. Your middle aged road trip will soon be a
+              tragedy that others joke about at your next party.
             </p>
-            <br/>
+            <br />
             <p>It doesn't have to be this way!</p>
           </div>
         </div>
       </div>
-      
-        {/* <h6>hello</h6> */}
-        {/* second video content */}
+
+      {/* <h6>hello</h6> */}
+      {/* second video content */}
       <div className={classes.Container}>
         <video autoPlay="autoplay" loop="loop" muted className={classes.Video}>
           <source src={secondVideo} type="video/mp4" />
@@ -43,12 +42,12 @@ const LandingPage = () => {
           <div className={classes.SubContent}>
             <h3>ChargeBuddy is your answer!</h3>
             <p className="container">
-            Charge Buddy offers geolocation as well as peer to peer networking allowing our users to update pricing information.  Locations of all  Charging Stations in your vicinity are displayed on your map, making sure that you arrive at your destination with your head held high. Once you create an account with us your information is securely stored on our server.  You will also be able to update pricing saving other unfortunates from being the punchline at Thanksgiving.
+              Charge Buddy offers geolocation as well as peer to peer networking allowing our users to update pricing information.  Locations of all  Charging Stations in your vicinity are displayed on your map, making sure that you arrive at your destination with your head held high. Once you create an account with us your information is securely stored on our server.  You will also be able to update prices; saving other unfortunates from being the punchline at Thanksgiving.
             </p>
-            <br/>
+            <br />
             <p> Charge Buddy is not the friend you want but the friend you need!</p>
             <button type="button">
-            <a href="/map">START NOW</a>
+              <a href="/map">START NOW</a>
             </button>
             {/* <a className="waves-effect waves-light btn-large orange darken-2">START NOW</a> */}
           </div>
