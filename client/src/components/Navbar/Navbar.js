0c4c71f6e6b8d11440c83240f28f7b12f74c8fda
Raffee <Raffee1989@gmail.com>
2021-06-01 20:09:33 -0700

    ChargeBuddy on Navbar will take us to home page

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
         <nav className="grey darken-3">
             <div className="nav-wrapper">
-                <a href="/" className="brand-logo"><i className="large material-icons">ev_station</i>ChargeBuddy</a>
+                <a href="/landing" className="brand-logo"><i className="large material-icons">ev_station</i>ChargeBuddy</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="/login"><i className="material-icons">person</i></a></li>
                 </ul>
