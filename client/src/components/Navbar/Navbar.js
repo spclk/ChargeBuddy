f7804ed3a67281f66291bde3ee94ebcb7197778a
Konstantin <konstantin1020@gmail.com>
2021-06-03 00:50:06 -0700

    uniform color theme

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
         <nav className="nav">
-            <div className="nav-wrapper">
+            <div className="nav-wrapper hide-on-small-only">
                 <a href="/" className="brand-logo lime-text text-darken-2"><i className="large material-icons prefix">ev_station</i>ChargeBuddy</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="/login"><i className="material-icons">person</i></a></li>
