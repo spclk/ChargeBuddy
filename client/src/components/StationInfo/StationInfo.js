33746a019d0ae19a0b91a565de689f3536caa5f4
warren clark <wclarkcito@gmail.com>
2021-06-03 21:26:51 -0700

    merge

--- client/src/components/StationInfo/StationInfo.js
+++ client/src/components/StationInfo/StationInfo.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import React, { useContext } from "react";
-import AuthContext from '../../utils/authContext'
-=======
-import React, {useContext} from "react";
 import AuthContext from '../../utils/authContext';
 import "./StationInfo.css";
->>>>>>> 1d4336172dc37366d68b11c807a56dd5b23b3b0e
 
 function StationInfo(props) {
     const { info } = props;
@@ -14,29 +9,12 @@ function StationInfo(props) {
     const stationStreet = `${info.city}, ${info.state} ${info.zip}`;
     const ports = `Ports: ${info.portTypes}`;
 
-<<<<<<< HEAD
 
-
-    const { authData } = useContext(AuthContext)
-
-    return (
-        <div>
-
-            {authData.isLoggedIn ?
-                <h5>user is logged in</h5>
-                : <h5>user is not logged in</h5>
-            }
-
-            <h5>{name}</h5>
-            {station}
-
-=======
-    
     // const {authData} = useContext(AuthContext)
 
     return (
         <div>
-            
+
             {/* {authData.isLoggedIn ?
             <h5>user is logged in</h5>
             : <h5>user is not logged in</h5>
@@ -46,8 +24,7 @@ function StationInfo(props) {
             <p className="station">{stationAddy}</p>
             <p className="station">{stationStreet}</p>
             <p className="station">{ports}</p>
-            
->>>>>>> 1d4336172dc37366d68b11c807a56dd5b23b3b0e
+
         </div>
     )
 }
