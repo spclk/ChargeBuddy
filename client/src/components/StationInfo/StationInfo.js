a62050eec610e362e8a837f548405c1ebadc5420
EJ <ej.roux13@gmail.com>
2021-06-04 17:58:12 -0700

    added pricing and cleaned up unused code

--- client/src/components/StationInfo/StationInfo.js
+++ client/src/components/StationInfo/StationInfo.js
@@ -1,5 +1,4 @@
-import React, { useContext } from "react";
-import AuthContext from '../../utils/authContext';
+import React from "react";
 import "./StationInfo.css";
 
 function StationInfo(props) {
@@ -8,23 +7,20 @@ function StationInfo(props) {
     const stationAddy = `${info.address}`;
     const stationStreet = `${info.city}, ${info.state} ${info.zip}`;
     const ports = `Ports: ${info.portTypes}`;
-
-
-    // const {authData} = useContext(AuthContext)
+    const format = (num) => num.toLocaleString("en-us", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+    });
+    const price = Math.floor((Math.random() * 10 + 5));
+    const formatPrice = `Full Recharge: $${format(price)}`;
 
     return (
         <div>
-
-            {/* {authData.isLoggedIn ?
-            <h5>user is logged in</h5>
-            : <h5>user is not logged in</h5>
-            } */}
-
             <h5 className="name">{name}</h5>
             <p className="station">{stationAddy}</p>
             <p className="station">{stationStreet}</p>
             <p className="station">{ports}</p>
-
+            <p className="station">{formatPrice}</p>
         </div>
     )
 }
